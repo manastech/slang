6e138d0b53b2e5ff050630a755826d9eae0f184e
Gustavo Giráldez <ggiraldez@manas.tech>
2025-05-15 14:01:38 -0400

    Changes to type modelling from the annotate-types branch

--- crates/solidity/outputs/cargo/crate/src/backend/types.rs
+++ crates/solidity/outputs/cargo/crate/src/backend/types.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
 use indexmap::IndexSet;
-use metaslang_cst::nodes::NodeId;
+
+use crate::cst::NodeId;
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct TypeId(usize);
@@ -9,13 +10,44 @@ pub struct TypeId(usize);
 pub struct TypeRegistry {
     types: IndexSet<Type>,
     definitions: HashMap<NodeId, TypeDefinition>,
+
+    // Pre-defined core types
+    address_type_id: TypeId,
+    bool_type_id: TypeId,
+    error_type_id: TypeId,
+    rational_type_id: TypeId,
+    string_type_id: TypeId,
+    uint256_type_id: TypeId,
+    void_type_id: TypeId,
 }
 
 impl TypeRegistry {
     fn new() -> Self {
+        let mut types = IndexSet::new();
+        let (address_type, _) = types.insert_full(Type::Address { payable: false });
+        let (bool_type, _) = types.insert_full(Type::Boolean);
+        let (error_type, _) = types.insert_full(Type::Error { node_id: None });
+        let (rational_type, _) = types.insert_full(Type::Rational);
+        let (string_type, _) = types.insert_full(Type::String {
+            location: DataLocation::Memory,
+        });
+        let (uint256_type, _) = types.insert_full(Type::Integer {
+            signed: false,
+            bits: 256,
+        });
+        let (void_type, _) = types.insert_full(Type::Void);
+
         Self {
-            types: IndexSet::new(),
+            types,
             definitions: HashMap::new(),
+
+            address_type_id: TypeId(address_type),
+            bool_type_id: TypeId(bool_type),
+            error_type_id: TypeId(error_type),
+            rational_type_id: TypeId(rational_type),
+            string_type_id: TypeId(string_type),
+            uint256_type_id: TypeId(uint256_type),
+            void_type_id: TypeId(void_type),
         }
     }
 
@@ -55,6 +87,83 @@ impl TypeRegistry {
             }
         }
     }
+
+    pub fn implicitly_convertible_to(&self, from_type_id: TypeId, to_type_id: TypeId) -> bool {
+        if from_type_id == to_type_id {
+            return true;
+        }
+        let from_type = self.get_type_by_id(from_type_id).unwrap();
+        let to_type = self.get_type_by_id(to_type_id).unwrap();
+
+        match (from_type, to_type) {
+            (
+                Type::Address {
+                    payable: from_payable,
+                },
+                Type::Address { .. },
+            ) => *from_payable,
+
+            (
+                Type::Integer {
+                    signed: from_signed,
+                    bits: from_bits,
+                },
+                Type::Integer {
+                    signed: to_signed,
+                    bits: to_bits,
+                },
+            ) => {
+                if from_signed == to_signed {
+                    from_bits <= to_bits
+                } else if *from_signed {
+                    false
+                } else {
+                    from_bits < to_bits
+                }
+            }
+
+            (Type::Rational, Type::Integer { .. }) => true,
+
+            // TODO: add more implicit conversion rules
+            _ => unimplemented!(
+                "implicitly converting from {from_type:?} to {to_type:?} not supported"
+            ),
+        }
+    }
+}
+
+impl TypeRegistry {
+    pub fn address(&self) -> TypeId {
+        self.address_type_id
+    }
+    pub fn bool(&self) -> TypeId {
+        self.bool_type_id
+    }
+    pub fn error(&self) -> TypeId {
+        self.error_type_id
+    }
+    pub fn rational(&self) -> TypeId {
+        self.rational_type_id
+    }
+    pub fn string(&self) -> TypeId {
+        self.string_type_id
+    }
+    pub fn uint256(&self) -> TypeId {
+        self.uint256_type_id
+    }
+    pub fn void(&self) -> TypeId {
+        self.void_type_id
+    }
+}
+
+impl TypeRegistry {
+    pub fn iter_types(&self) -> impl Iterator<Item = (TypeId, &Type)> {
+        (0usize..).map(TypeId).zip(self.types.iter())
+    }
+
+    pub fn iter_definitions(&self) -> impl Iterator<Item = &TypeDefinition> {
+        self.definitions.values()
+    }
 }
 
 impl Default for TypeRegistry {
@@ -67,6 +176,7 @@ impl Default for TypeRegistry {
 pub enum TypeDefinition {
     Contract(ContractTypeDefinition),
     Enum(EnumTypeDefinition),
+    Error(ErrorTypeDefinition),
     Interface(InterfaceTypeDefinition),
     Struct(StructTypeDefinition),
     UserDefinedValueType(UserDefinedValueTypeDefinition),
@@ -77,6 +187,7 @@ impl TypeDefinition {
         match self {
             Self::Contract(ContractTypeDefinition { node_id, .. })
             | Self::Enum(EnumTypeDefinition { node_id, .. })
+            | Self::Error(ErrorTypeDefinition { node_id, .. })
             | Self::Interface(InterfaceTypeDefinition { node_id, .. })
             | Self::Struct(StructTypeDefinition { node_id, .. })
             | Self::UserDefinedValueType(UserDefinedValueTypeDefinition { node_id, .. }) => {
@@ -134,11 +245,26 @@ pub struct EnumMember {
 }
 
 #[derive(Debug, Eq, PartialEq)]
+pub struct ErrorTypeDefinition {
+    pub node_id: NodeId,
+    pub name: String,
+    pub members: Vec<ErrorField>,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct ErrorField {
+    pub node_id: NodeId,
+    pub name: Option<String>,
+    pub type_id: TypeId,
+}
+
+#[derive(Debug, Eq, PartialEq)]
 pub struct UserDefinedValueTypeDefinition {
     pub node_id: NodeId,
     pub name: String,
     pub type_id: TypeId,
 }
+
 #[derive(Debug, Eq, Hash, PartialEq)]
 pub enum Type {
     Address {
@@ -162,6 +288,9 @@ pub enum Type {
     Enum {
         node_id: NodeId,
     },
+    Error {
+        node_id: Option<NodeId>,
+    },
     FixedPointNumber {
         signed: bool,
         bits: u32,
@@ -169,7 +298,7 @@ pub enum Type {
     },
     Function {
         parameter_types: Vec<TypeId>,
-        return_types: Vec<TypeId>,
+        return_type: TypeId,
         external: bool,
         kind: FunctionTypeKind,
     },
@@ -186,6 +315,7 @@ pub enum Type {
         value_name: Option<String>,
         value_type_id: TypeId,
     },
+    Rational,
     String {
         location: DataLocation,
     },
@@ -193,9 +323,16 @@ pub enum Type {
         node_id: NodeId,
         location: DataLocation,
     },
+    Tuple {
+        types: Vec<TypeId>,
+    },
+    Undecided {
+        choices: Vec<TypeId>,
+    },
     UserDefinedValueType {
         node_id: NodeId,
     },
+    Void,
 }
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -234,11 +371,16 @@ impl Type {
             | Self::ByteArray { .. }
             | Self::Contract { .. }
             | Self::Enum { .. }
+            | Self::Error { .. }
             | Self::Function { .. }
             | Self::FixedPointNumber { .. }
             | Self::Integer { .. }
             | Self::Interface { .. }
-            | Self::UserDefinedValueType { .. } => None,
+            | Self::Rational
+            | Self::Tuple { .. }
+            | Self::Undecided { .. }
+            | Self::UserDefinedValueType { .. }
+            | Self::Void => None,
             Self::Mapping { .. } => Some(DataLocation::Storage),
             Self::Array { location, .. } => Some(*location),
             Self::Bytes { location } => Some(*location),
